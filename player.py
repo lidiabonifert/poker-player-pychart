aaecec9585b1923104f5ffe6cb7e5402e8c02095
Lidia Bonifert <bonifert.lidia@gmail.com>
2018-02-19 11:29:17 +0100

    fixed syntax

--- player.py
+++ player.py
@@ -1,17 +1,30 @@
+import json
 
 
 class Player:
     VERSION = "All in!!"
 
+    def get_self(self, current_game):
+        for i in range(0, len(current_game["players"])):
+            if current_game["players"][i]["name"] == "PyChart":
+                return current_game["players"][i]
+
+    def get_cards(self, self_data):
+        first_card = self_data["hole_cards"][0]
+        second_card = self_data["hole_cards"][1]
+        return first_card, second_card
+
+    def get_stack(self, self_data):
+        return self_data["stack"]
+
     def betRequest(self, game_state):
         current_game = json.loads(game_state)
-        for i in range(0, len(current_game[players])):
-            if current_game[players][i][name] == "PyChart":
-                first_rank = current_game[players][i][hole_cards][0][rank]
-                second_rank = current_game[players][i][hole_cards][1][rank]
-                if first_rank == second_rank:
-                    return 100
-        return 1000
+        self_data = get_self(current_game)
+        current_stack = get_stack(self_data)
+        received_hand = get_cards(self_data)
+        if received_hand[0]["rank"] == received_hand[1]["rank"]:
+            return current_stack
+        return 0
 
     def showdown(self, game_state):
         pass
