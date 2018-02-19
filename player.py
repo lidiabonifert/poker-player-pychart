94e8464f93870da468d69ada5764c0c229f5bc57
Lidia Bonifert <bonifert.lidia@gmail.com>
2018-02-19 10:51:44 +0100

    first try

--- player.py
+++ player.py
@@ -4,6 +4,13 @@ class Player:
     VERSION = "All in!!"
 
     def betRequest(self, game_state):
+        current_game = json.loads(game_state)
+        for i in range(0, len(current_game[players])):
+            if current_game[players][i][name] == "PyChart":
+                first_rank = current_game[players][i][hole_cards][0][rank]
+                second_rank = current_game[players][i][hole_cards][1][rank]
+                if first_rank == second_rank:
+                    return 100
         return 1000
 
     def showdown(self, game_state):
