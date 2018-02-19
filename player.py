75caba635973b3d59937c33233d7c7647a327367
Kata <katalin.molnar.kiss@gmail.com>
2018-02-19 13:40:34 +0100

    v0.1

--- player.py
+++ player.py
@@ -2,12 +2,17 @@ import json
 # import sys
 
 
-class Player:
-    VERSION = "Error check"
+class Player:   
+    VERSION = "0.1"
 
-    def get_cards(self, game_state):
-        first_card = game_state["hole_cards"][0]
-        second_card = game_state["hole_cards"][1]
+    def get_self(self, game_state):
+            for i in range(0, len(game_state["players"])):
+                if game_state["players"][i]["name"] == "PyChart":
+                    return game_state["players"][i]
+
+    def get_cards(self, get_self):
+        first_card = get_self["hole_cards"][0]
+        second_card = get_self["hole_cards"][1]
         return first_card, second_card
 
     def get_stack(self, game_state):
@@ -15,17 +20,20 @@ class Player:
 
     def betRequest(self, game_state):
         print("*********************")
-        print(game_state)
-            #print(game_state[i])
-            
-        
-        # current_game = game_state  # json.loads(game_state)
-        # self_data = get_self(current_game)
-        # current_stack = get_stack(self_data)
-        # received_hand = get_cards(self_data)
-        # if received_hand[0]["rank"] == received_hand[1]["rank"]:
-          #  return 1000
-        return 1000
+        print(get_self["name"])
+        self_data = Player.get_self(game_state)
+        received_hand = Player.get_cards(self_data)
+        print("self data:")
+        print(self_data)
+        first_card = received_hand[0]
+        print("first card: ")
+        print(first_card)
+        second_card = received_hand[1]
+        print("second card")
+        print(second_card)
+        if first_card["rank"] == second_card["rank"]:
+            return 1000
+        return 0
 
     def showdown(self, game_state):
         pass
